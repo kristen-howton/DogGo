ce5b978915fa68dce8ebfb2adc62504cc911bbe6
Kristen Howton <kristen.howton@gmail.com>
2020-06-17 19:05:06 -0500

    set up a utlity for updating for null and reader of null values can now edit dog, delete dog, edit owner, and delete owner

---
+++ DogGo/DogGo/Utils/UpdateNullable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Utils
+{
+    public class UpdateNullable
+    {
+        public static void SetNullableString(SqlCommand cmd, string param, string value)
+        {
+            if (value == null)
+            {
+                cmd.Parameters.AddWithValue(param, DBNull.Value);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue(param, value);
+            }
+        }
+
+    }
+}
